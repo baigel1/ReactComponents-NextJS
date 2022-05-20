44b4c5e7f92b7e1a3447f38c1d760cbc33d33e26
Daniel <dbaigel@yext.com>
2022-05-20 12:17:51 -0400

    moved headless provider to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AnswersHeadlessProvider } from "@yext/answers-headless-react";
+import { answersSandboxEndpoints } from "../answersConfig";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AnswersHeadlessProvider
+      experienceKey="test-experience"
+      apiKey="9641a03ad3de221fb9c566b0c9148351"
+      locale="en"
+      endpoints={answersSandboxEndpoints}
+    >
+      <Component {...pageProps} />
+    </AnswersHeadlessProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,14 @@ import type { NextPage } from "next";
 import SearchStuff from "../Components/SearchStuff";
 import React from "react";
 import ReactDOM from "react-dom";
-import { AnswersHeadlessProvider } from "@yext/answers-headless-react";
-import { answersSandboxEndpoints } from "../answersConfig";
+
+
 
 const Home: NextPage = () => {
   return (
-    <AnswersHeadlessProvider
-      experienceKey="test-experience"
-      apiKey="9641a03ad3de221fb9c566b0c9148351"
-      locale="en"
-      endpoints={answersSandboxEndpoints}
-    >
+   
       <SearchStuff />
-    </AnswersHeadlessProvider>
+   
   );
 };
 
